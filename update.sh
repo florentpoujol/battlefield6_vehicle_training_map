6bc799fbf8778c09843fe89f867ca5e5b02571bb
Florent Poujol <florentpoujol@fastmail.com>
2025-10-13 22:49:52 +0200

    update for v10 (added static tank shooting range)

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-cp G:\\bf6_portal\\export\\levels\\MP_Tungsten_Vehicle_Training.spatial.json G:\\code\\bf6_vehicle_training_map\\MP_Tungsten_Vehicle_Training.spatial.json
+cp G:\\bf6_portal\\export\\levels\\MP_Tungsten_Vehicle_Training.spatial.json G:\\code\\bf6_vehicle_training_map\\src\\MP_Tungsten_Vehicle_Training.spatial.json
 
-cp G:\\bf6_portal\\GodotProject\\levels\\MP_Tungsten_Vehicle_Training.tscn G:\\code\\bf6_vehicle_training_map\\MP_Tungsten_Vehicle_Training.tscn
+cp G:\\bf6_portal\\GodotProject\\levels\\MP_Tungsten_Vehicle_Training.tscn G:\\code\\bf6_vehicle_training_map\\src\\MP_Tungsten_Vehicle_Training.tscn
